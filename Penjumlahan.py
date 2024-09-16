137a164520b319c39c5748c5351ce6d7a84d8a3f
Asfal Fuad <fuadasfal32@gmail.com>
2024-09-16 23:11:34 +0800

    Create Penjumlahan.py

---
+++ Penjumlahan.py
@@ -0,0 +1,5 @@
+angka1 = input (" Masukan angka pertama : ")
+angka2 = input (" Masukan angka kedua : ")
+
+sum = int(angka1) + int(angka2)
+print("hasil penjumlahan dari {0} + {1} adalah {2}".format(angka1, angka2, sum))
